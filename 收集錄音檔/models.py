7479f96490bb954e7572a8185889fb9c8cdffec2
薛丞宏 <ihcaoe@gmail.com>
2016-08-20 16:42:50 +0800

    整理文案，予音檔會當傳一擺就好

--- 收集錄音檔/models.py
+++ 收集錄音檔/models.py
@@ -6,14 +6,14 @@ class 語料表(models.Model):
     收錄時間 = models.DateField(auto_now_add=True)
     上尾修改時間 = models.DateField(auto_now=True)
 
-    漢字 = models.TextField()
-    臺羅 = models.TextField()
-    音檔 = models.FileField()
+    漢字 = models.TextField(blank=True)
+    臺羅 = models.TextField(blank=True)
+    音檔 = models.FileField(blank=True)
 
     備註 = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
-        return '{} {}'.format(self.漢字.split()[0], self.備註)
+        return '{} {}'.format(''.join(self.漢字.split()[:1]), self.備註)
 
 
 class 語料表格(ModelForm):
